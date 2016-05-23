ed5590e369232b52b72b6b86f35abad63b7d3082
117rinari <railgun117@yahoo.co.jp>
2016-05-23 16:27:10 +0900

    コミットテスト

---
+++ 実習/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  実習
+//
+//  Created by kanade on 2016/05/23.
+//  Copyright © 2016年 mycompany. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Task: UIViewController {
+    
+}
