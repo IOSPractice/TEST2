446eb142bece75c0ea1d6a00b107e6a3e1e25dee
kanta <metoro667@gmail.com>
2016-05-30 09:34:48 +0900

    バグを修正

--- 実習/Task.swift
+++ 実習/Task.swift
@@ -1,8 +1,9 @@
+
 //
-//  Task.swift
+//  File.swift
 //  実習
 //
-//  Created by kanade on 2016/05/23.
+//  Created by 渡辺寛太 on 2016/05/30.
 //  Copyright © 2016年 mycompany. All rights reserved.
 //
 
@@ -10,5 +11,15 @@ import Foundation
 import UIKit
 
 class Task: UIViewController {
+
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
