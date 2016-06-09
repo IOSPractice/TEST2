4285fad183efd4a9ebee069a1179ced3bf2613d8
tamago0224 <bigtreesasuke200733@gmail.com>
2016-06-10 00:53:59 +0900

    セルの管理画面の追加とControllerの追加

---
+++ 実習/TimeTableEditController.swift
@@ -0,0 +1,16 @@
+//
+//  TimeTableEditController.swift
+//  実習
+//
+//  Created by 大木一輝 on 2016/06/10.
+//  Copyright © 2016年 mycompany. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class TimeTableEditController: UIViewController {
+    @IBOutlet weak var className: UITextField!
+    @IBOutlet weak var classNum: UITextField!
+    
+}
