7a4e299ddef75cff629c72f0afc114a38fbb86fa
tamago0224 <bigtreesasuke200733@gmail.com>
2016-07-06 16:32:53 +0900

    詳細設定

--- 実習/DetailSettingViewController.swift
+++ 実習/DetailSettingViewController.swift
@@ -12,8 +12,14 @@ import UIKit
 class DetailSettingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var detailTableView: UITableView!
     
+    let stationTitles = ["高坂", "北坂戸", "熊谷"]
+    
+    var takasakaSwch: UISwitch!
+    var kitasakadoSwch: UISwitch!
+    var kumagayaSwch: UISwitch!
+    var notifSwch: UISwitch!
+    
     override func viewDidLoad() {
-        self.view.backgroundColor = UIColor.whiteColor()
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -22,8 +28,54 @@ class DetailSettingViewController: UIViewController, UITableViewDelegate, UITabl
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("CellDetail")
-        cell?.textLabel?.text = "\(indexPath.row)"
+        
+        switch indexPath.section {
+        case 0:
+            cell?.textLabel?.text = "\(stationTitles[indexPath.row])"
+            
+            switch indexPath.row {
+            case 0:
+                takasakaSwch = UISwitch()
+                //TODO:takasakaSwch.on = true
+                takasakaSwch.addTarget(self, action: #selector(DetailSettingViewController.switchAction(_:)), forControlEvents: .ValueChanged)
+                cell?.accessoryView = UIView(frame: takasakaSwch.frame)
+                cell?.accessoryView?.addSubview(takasakaSwch)
+            case 1:
+                kitasakadoSwch = UISwitch()
+                //TODO:kitasakadoSwch.on = true
+                kitasakadoSwch.addTarget(self, action: #selector(DetailSettingViewController.switchAction(_:)), forControlEvents: .ValueChanged)
+                cell?.accessoryView = UIView(frame: kitasakadoSwch.frame)
+                cell?.accessoryView?.addSubview(kitasakadoSwch)
+            case 2:
+                kumagayaSwch = UISwitch()
+                //TODO:kumagayaSwch.on = true
+                kumagayaSwch.addTarget(self, action: #selector(DetailSettingViewController.switchAction(_:)), forControlEvents: .ValueChanged)
+                cell?.accessoryView = UIView(frame: kumagayaSwch.frame)
+                cell?.accessoryView?.addSubview(kumagayaSwch)
+
+            default:
+                break
+            }
+            
+        case 1:
+            notifSwch = UISwitch()
+            notifSwch.addTarget(self, action: #selector(DetailSettingViewController.notificationSwitching(_:)), forControlEvents: .ValueChanged)
+            //TODO: notifSwch.on = true
+            cell?.accessoryView = UIView(frame: notifSwch.frame)
+            cell?.accessoryView?.addSubview(notifSwch)
+        default:
+            break
+        }
+        
+        
         return cell!
     }
     
+    func switchAction(sender: UISwitch) {
+        
+    }
+    
+    func notificationSwitching(sender: UISwitch) {
+        
+    }
 }
